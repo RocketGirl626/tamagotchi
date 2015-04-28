6341799d00b0ade77c3565412a8d6a654311f6a8
jeanette fairless & jocelyn alsdorf <student@epicodus.com>
2015-04-28 13:44:55 -0700

    set instance variable and define  associated instance methods

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,5 +1,23 @@
 class Tamagotchi
   define_method(:initialize) do |name|
-    
+    @name = name
+    @food_level = 10
+    @sleep_level = 10
+    @activity_level = 10
+  end
+  define_method(:name) do
+    @name
+  end
+  define_method(:food_level) do
+    @food_level
+  end
+  define_method(:sleep_level) do
+    @sleep_level
+  end
+  define_method(:activity_level) do
+    @activity_level
+  end
+  define_method(:time_passes) do
+    @food_level = @food_level.-(1)
   end
 end
